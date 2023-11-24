f2caaa78ddf52713c1249cf2e327cffa9619eb5f
TopskiyAccount <125979547+TopskiyAccount@users.noreply.github.com>
2023-11-24 22:00:57 +0300

    Add files via upload

--- pract/mod10/task1.py
+++ pract/mod10/task1.py
@@ -3,40 +3,60 @@ import doctest
 
 def is_valid_password(password):
     """
-    Проверяет, является ли входная строка корректным паролем.
+    Проверяет, является ли пароль password допустимым.
 
-    Пароль должен:
-    - Содержать только латинские символы, цифры и специальные символы ^$%@#&*
-    - Состоять из не менее чем восьми символов
-    - Содержать по крайней мере два латинских символа в нижнем регистре
-    - Содержать по крайней мере одну цифру
-    - Содержать по крайней мере три различных специальных символа
-    - Не содержать символы ,.!?
+    Args:
+    - password (str): Пароль для проверки.
 
-    Примеры:
+    Returns:
+    - bool: True, если пароль соответствует критериям, иначе False.
+
     >>> is_valid_password("rtG&3FG#Tr^e")
     True
+    
     >>> is_valid_password("a^A1@*@1Aa")
     True
+    
     >>> is_valid_password("oF^a1D@y5%e6")
     True
-    >>> is_valid_password("enroi#$*rkdeR#$*092uwedchf34tguv394h")
+    
+    >>> is_valid_password("enroi#$*rkdeR#$*092uwedchf34tguv394hNoSpecialChars1")
     True
+    
     >>> is_valid_password("пароль")
     False
+    
     >>> is_valid_password("password")
     False
+        
     >>> is_valid_password("qwerty")
     False
+        
     >>> is_valid_password("lOngPa$$W0Rd")
     False
     """
 
-    pattern = r"^(?=.*[a-z].*[a-z])(?=.*\d)(?=.*[^a-zA-Z\d])(?!.*[ ,.!?]).{8,}$"
 
-    return bool(re.match(pattern, password))
+    if not re.search(r'[a-z].*[a-z]', password):
+        return False
+    
+
+    if not re.search(r'\d', password):
+        return False
+    
+
+    if not re.search(r'(?:[^$%^@#&*]*[$%^@#&*]){3}', password):
+        return False
+    
+   
+    if re.search(r'[,.!?]', password):
+        return False
+    
+
+    if not re.match(r'[a-zA-Z0-9$%^@#&*]{8,}$', password):
+        return False
+    
+    return True
 
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+doctest.testmod(verbose=True)
